fa0c3b8dff87c1311e93c66b520c46cba0791f1d
Sneharsh Belsare <snehbelsare@gmail.com>
2021-06-02 22:34:40 +0530

    Updated homepage

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Footer from "../../components/Footer/Footer";
-import PlacesCard from "../../components/PlacesCard/PlacesCard";
 import HomeCss from "./Home.module.css";
-import Img from "./unsplash-david-marcu.jpg";
 
 function Home() {
     return (
@@ -21,7 +19,7 @@ function Home() {
                 </nav>
                 <main>
                     <div
-                        data-aos="fade-up"
+                        data-aos="zoom-in"
                         className={HomeCss.cardContainer}
                         style={{ mixBlendMode: "screen" }}
                     >
@@ -29,47 +27,73 @@ function Home() {
                             <h1>Welcome to Nirvana</h1>
                         </div>
                     </div>
-                    <div data-aos="fade-up" className={HomeCss.quote}>
+                    <div className={HomeCss.quote}>
                         "Jobs fill your pockets, adventures fill your soul."
                     </div>
                     <div className={HomeCss.body}>
-                        <div className={HomeCss.placesContainer}>
-                            <h1>Places to explore :</h1>
+                        <div className={HomeCss.featuresContainer}>
+                            <div
+                                data-aos="fade-right"
+                                className={HomeCss.featuresCard}
+                            >
+                                <h1>Explore</h1>
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur
+                                    adipisicing elit. Magni, incidunt quas.
+                                    Dolorum fuga facilis eveniet vero illo?
+                                    Repudiandae, ipsam voluptas.
+                                </p>
+                            </div>
+                            <div
+                                data-aos="fade-down"
+                                className={HomeCss.featuresCard}
+                            >
+                                <h1>Discover</h1>
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur
+                                    adipisicing elit. Magni, incidunt quas.
+                                    Dolorum fuga facilis eveniet vero illo?
+                                    Repudiandae, ipsam voluptas.
+                                </p>
+                            </div>
+                            <div
+                                data-aos="fade-left"
+                                className={HomeCss.featuresCard}
+                            >
+                                <h1>Enjoy</h1>
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur
+                                    adipisicing elit. Magni, incidunt quas.
+                                    Dolorum fuga facilis eveniet vero illo?
+                                    Repudiandae, ipsam voluptas.
+                                </p>
+                            </div>
+                        </div>
+                        <div class={HomeCss.CTA}>
+                            <h1>
+                                Nirvana brings you closer to the best nature has
+                                to offer in three simple steps !!
+                            </h1>
                             <ul>
-                                <li>
-                                    <PlacesCard
-                                        rtl={false}
-                                        img={Img}
-                                        title={"Switzerland"}
-                                        desc={
-                                            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam ullam fuga, quam ducimus amet ipsa consequuntur et. Nulla, fuga eos"
-                                        }
-                                    />
+                                <li data-aos="zoom-in">
+                                    1. Select a destination.
                                 </li>
-                                <li>
-                                    <PlacesCard
-                                        rtl={true}
-                                        img={Img}
-                                        title={"Switzerland"}
-                                        desc={
-                                            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam ullam fuga, quam ducimus amet ipsa consequuntur et. Nulla, fuga eos"
-                                        }
-                                    />
+                                <li data-aos="zoom-in" data-aos-delay="500">
+                                    2. Tailor a package as per your need.
                                 </li>
-                                <li>
-                                    <PlacesCard
-                                        rtl={false}
-                                        img={Img}
-                                        title={"Switzerland"}
-                                        desc={
-                                            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam ullam fuga, quam ducimus amet ipsa consequuntur et. Nulla, fuga eos"
-                                        }
-                                    />
+                                <li data-aos="zoom-in" data-aos-delay="1000">
+                                    3. Cherish !!
                                 </li>
                             </ul>
-                            <div className={HomeCss.morebtn}>
-                                <a href="/#">See all</a>
-                            </div>
+                        </div>
+                        <div class={HomeCss.btt}>
+                            <button
+                                onClick={() =>
+                                    (document.documentElement.scrollTop = 0)
+                                }
+                            >
+                                Back to top
+                            </button>
                         </div>
                     </div>
                 </main>
